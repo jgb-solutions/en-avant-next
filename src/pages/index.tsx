820ccd3f79fcdb83739405821da0124874f8238b
Jean Gérard Bousiquot <jgbneatdesign@gmail.com>
2020-06-17 19:24:12 -0400

    Homepage almost done.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Button from 'react-bootstrap/Button'
+import { FaPaperPlane, FaEnvelope, FaPhone, FaMapMarker, FaFacebook, FaTwitter, FaInstagram, FaFacebookF } from 'react-icons/fa'
 
 import SEO from 'components/SEO'
 import { APP_NAME } from 'utils/constants'
@@ -17,7 +18,7 @@ export default function Index() {
       <SEO />
       <section className="section-container">
         <Container>
-          <NavBar />
+          {/* <NavBar /> */}
         </Container>
       </section>
 
@@ -77,10 +78,10 @@ export default function Index() {
         </Container>
       </section>
 
-      <section className="section-container">
-        <Container style={{
-          marginTop: -150
-        }}>
+      <section className="section-container" style={{
+        paddingBottom: 30
+      }}>
+        <Container style={{ marginTop: -150 }}>
           <Row noGutters style={{
             boxShadow: "0px 5px 7px 0px rgba(0,0,0,0.35)"
           }}>
@@ -161,8 +162,7 @@ export default function Index() {
                       textTransform: "uppercase",
                       color: Colors.blue,
                       marginBottom: 12
-                    }}>Respect du mandat de Jovenel
-Moïse: Kelly Bastien </h3>
+                    }}>Respect du mandat de Jovenel Moïse: Kelly Bastien </h3>
                     <p style={{
                       color: Colors.gray,
                       fontSize: 12,
@@ -174,15 +174,287 @@ Moïse: Kelly Bastien </h3>
             ))}
           </Row>
         </Container>
-      </section >
-      <section className="section-container">
-        <Container>changer</Container>
       </section>
       <section className="section-container">
-        <Container>soutien</Container>
+        <Image src="/assets/images/hands.png"
+          style={{ width: "100%" }}
+          fluid />
       </section>
-      <section className="section-container">
-        <Container>footer</Container>
+      <section className="section-container" style={{
+        paddingTop: 150,
+        paddingBottom: 150
+      }}>
+        <Container>
+          <Row>
+            <Col md={5} style={{}}>
+              <h2 style={{
+                fontSize: 25,
+                fontWeight: 'bold',
+                color: Colors.blue,
+                textTransform: "uppercase",
+              }}>Nous comptons sur votre <br /> <span style={{ color: Colors.orange }}>soutien</span></h2>
+
+              <p style={{ color: Colors.gray }}>Faites un pour renforcer votre action sur le terrain</p>
+
+              <div>
+                <Button style={{
+                  borderRadius: 20,
+                  backgroundColor: Colors.blue,
+                  color: Colors.white,
+                  textTransform: "uppercase",
+                  paddingLeft: 24,
+                  paddingRight: 24,
+                }}>Adherer</Button>
+
+              &nbsp;
+              &nbsp;
+              &nbsp;
+
+              <Button style={{
+                  borderRadius: 20,
+                  backgroundColor: Colors.orange,
+                  color: 'white',
+                  textTransform: "uppercase",
+                  paddingLeft: 24,
+                  paddingRight: 24,
+                }}>donner</Button>
+              </div>
+            </Col>
+            <Col md={7} style={{}}>
+              <Row>
+                <Col sm={6} style={{ marginBottom: 60 }}>
+                  <h4 style={{
+                    color: Colors.orange,
+                    marginBottom: 20
+                  }}>
+                    <span style={{
+                      boxShadow: "0px 0px 5px 5px rgba(223,52,71,0.15)",
+                      borderRadius: '50%',
+                      padding: 8,
+                      fontSize: 12,
+                      fontWeight: 'bold',
+                      marginRight: 12,
+                    }}>
+                      <FaPaperPlane style={{}} />
+                    </span>
+                    <span style={{
+
+                    }}>Notre Manifeste</span>
+                  </h4>
+                  <p style={{
+                    fontSize: 12
+                  }}>
+                    Lorem ipsum dolor sit amet, conset adou adipisicing elit, sed do eiusm temporaryl incididunt ugt labore.
+                  </p>
+                </Col>
+                <Col sm={6} >
+                  <h4 style={{
+                    color: Colors.orange,
+                    marginBottom: 20
+                  }}>
+                    <span style={{
+                      boxShadow: "0px 0px 5px 5px rgba(223,52,71,0.15)",
+                      borderRadius: '50%',
+                      padding: 8,
+                      fontSize: 12,
+                      fontWeight: 'bold',
+                      marginRight: 12,
+                    }}>
+                      <FaPaperPlane style={{}} />
+                    </span>
+                    <span style={{
+
+                    }}>Notre Manifeste</span>
+                  </h4>
+                  <p style={{
+                    fontSize: 12
+                  }}>
+                    Lorem ipsum dolor sit amet, conset adou adipisicing elit, sed do eiusm temporaryl incididunt ugt labore.
+                  </p>
+                </Col>
+                <Col sm={6} >
+                  <h4 style={{
+                    color: Colors.orange,
+                    marginBottom: 20
+                  }}>
+                    <span style={{
+                      boxShadow: "0px 0px 5px 5px rgba(223,52,71,0.15)",
+                      borderRadius: '50%',
+                      padding: 8,
+                      fontSize: 12,
+                      fontWeight: 'bold',
+                      marginRight: 12,
+                    }}>
+                      <FaPaperPlane style={{}} />
+                    </span>
+                    <span style={{
+
+                    }}>Notre Manifeste</span>
+                  </h4>
+                  <p style={{
+                    fontSize: 12
+                  }}>
+                    Lorem ipsum dolor sit amet, conset adou adipisicing elit, sed do eiusm temporaryl incididunt ugt labore.
+                  </p>
+                </Col>
+                <Col sm={6} >
+                  <h4 style={{
+                    color: Colors.orange,
+                    marginBottom: 20
+                  }}>
+                    <span style={{
+                      boxShadow: "0px 0px 5px 5px rgba(223,52,71,0.15)",
+                      borderRadius: '50%',
+                      padding: 8,
+                      fontSize: 12,
+                      fontWeight: 'bold',
+                      marginRight: 12,
+                    }}>
+                      <FaPaperPlane style={{}} />
+                    </span>
+                    <span style={{
+
+                    }}>Notre Manifeste</span>
+                  </h4>
+                  <p style={{
+                    fontSize: 12
+                  }}>
+                    Lorem ipsum dolor sit amet, conset adou adipisicing elit, sed do eiusm temporaryl incididunt ugt labore.
+                  </p>
+                </Col>
+              </Row>
+            </Col>
+          </Row>
+
+        </Container>
+      </section>
+      <section className="section-container" style={{
+        backgroundColor: Colors.darkerBlue,
+        paddingTop: 80,
+        paddingBottom: 80,
+      }}>
+        <Container>
+          <Row>
+            <Col md={4}>
+              <p style={{
+                fontSize: 10,
+                color: Colors.darkerGray,
+                marginBottom: 45
+              }}>
+                “ D'aucuns pensent que le déclin continu de notre pays est inévitable
+                et que le pire est à venir. Les fondateurs du mouvement <strong>« EN AVANT »</strong> osent
+                prétendre autrement. En effet, une prise en charge de la politique par un
+                leadership collectif et visionnaire peut conduire Haïti sur la voie du
+                progrès et du développement”.
+              </p>
+
+              <div>
+                <span style={{
+                  fontSize: 11,
+                  backgroundColor: Colors.facebook,
+                  color: Colors.white,
+                  padding: 12,
+                  borderRadius: '50%'
+                }}>
+                  <FaFacebookF />
+                </span>
+                &nbsp;
+                &nbsp;
+                <span style={{
+                  fontSize: 11,
+                  backgroundColor: Colors.twitter,
+                  color: Colors.white,
+                  padding: 12,
+                  borderRadius: '50%'
+                }}>
+                  <FaTwitter />
+                </span>
+                &nbsp;
+                &nbsp;
+                <span style={{
+                  fontSize: 11,
+                  backgroundColor: Colors.instagram,
+                  color: Colors.white,
+                  padding: 12,
+                  borderRadius: '50%'
+                }}>
+                  <FaInstagram />
+                </span>
+              </div>
+            </Col>
+            <Col md={8}>
+              <Row>
+                <Col md={4}>
+                  <h5 style={{
+                    color: Colors.white,
+                    fontSize: 12,
+                    lineHeight: 1.3
+                  }}>NOTRE MOUVEMENT <br /> EN PROVINCE</h5>
+                  <ul style={{
+                    listStyle: "none",
+                    padding: 0,
+                    color: Colors.evenDarkerGray,
+                    textTransform: 'uppercase',
+                    fontSize: 10
+                  }}>
+                    <li>ouest</li>
+                    <li>centre</li>
+                    <li>nord-est</li>
+                    <li>nord</li>
+                    <li>artibonite</li>
+                    <li>sud</li>
+                    <li>sud-est</li>
+                    <li>grande-anse</li>
+                  </ul>
+                </Col>
+                <Col md={4}>
+                  <h5 style={{
+                    color: Colors.white,
+                    fontSize: 12,
+                    lineHeight: 1.3,
+                    textTransform: "uppercase"
+                  }}>NOTRE MOUVEMENT A L’ÉTRANGER (la DIASPORA)</h5>
+                  <ul style={{
+                    listStyle: "none",
+                    padding: 0,
+                    color: Colors.evenDarkerGray,
+                    textTransform: 'uppercase',
+                    fontSize: 10
+                  }}>
+                    <li>ouest</li>
+                    <li>centre</li>
+                    <li>nord-est</li>
+                    <li>nord</li>
+                    <li>artibonite</li>
+                    <li>sud</li>
+                    <li>sud-est</li>
+                    <li>grande-anse</li>
+                  </ul>
+                </Col>
+                <Col md={4}>
+                  <h5 style={{
+                    color: Colors.white,
+                    fontSize: 12,
+                    lineHeight: 1.3,
+                    textTransform: "uppercase"
+                  }}>Contact</h5>
+                  <ul style={{
+                    listStyle: "none",
+                    padding: 0,
+                    color: Colors.evenDarkerGray,
+                    textTransform: 'uppercase',
+                    fontSize: 11,
+                    lineHeight: 2
+                  }}>
+                    <li><FaMapMarker /> Location</li>
+                    <li><FaPhone /> (XXX) xxxx xxx</li>
+                    <li><FaEnvelope /> info@enavant.ht</li>
+                  </ul>
+                </Col>
+              </Row>
+            </Col>
+          </Row>
+        </Container>
       </section>
     </>
   )
--- src/utils/colors.ts
+++ src/utils/colors.ts
@@ -1,8 +1,17 @@
 const Colors = {
 	orange: "#f46425",
 	blue: "#044482",
+	darkerBlue: "#083375",
 	white: "#ffffff",
 	gray: "#777777",
+	darkerGray: "#d6d6d6",
+	evenDarkerGray: "#8e8e8e",
+	facebook: "#1877f2",
+	twitter: "#1da1f2",
+	youtube: "#ff0000",
+	instagram: "#c32aa3",
+	whatsapp: "#25d366",
+	telegram: "#0088cc",
 }
 
 export default Colors
